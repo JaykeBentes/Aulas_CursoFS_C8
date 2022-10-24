7f82e14817c2ddf649cceeef7cafcb65a6058f12
JaykeBentes <jakebentes@hotmail.com>
2022-10-24 19:04:57 -0400

    Última atualização da aula

--- Atividade5_UC8.js
+++ Atividade5_UC8.js
@@ -18,3 +18,14 @@ let nomeDeAlunos = ["Aluno1", "Aluno2", "Aluno3", "Aluno4"]
 for (let nome of nomeDeAlunos){
     console.log(`Esta pessoa se chama ${nome}`)
 }
+
+
+let acompanhado = false
+let idade = 33
+let ingresso = true
+
+if(idade >= 18 && ingresso == true || acompanhado == true) {
+    console.log("Bem vindo a festa!")
+}else {
+    console.log("Você não pode participar da festa, infelizmente.")
+}
