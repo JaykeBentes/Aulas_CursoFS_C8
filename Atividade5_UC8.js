170ddae53ebb4e93d686df1b80452866cbce0788
JaykeBentes <jakebentes@hotmail.com>
2022-10-24 18:30:37 -0400

    atualização segunda parte da aula

--- Atividade5_UC8.js
+++ Atividade5_UC8.js
@@ -1,22 +1,15 @@
-/*
-Percorrer de Zero até o número de alunos
-
-Se o número for par, escreva ‘par’ e o número correspondente
-
-Se o número for ímpar, escreva ‘ímpar’ e o número correspondente  
-
-Se o número for zero, escreva ‘zero’ e o número correspondente
-*/
 
 let numeroDeAlunos = 12
-for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+let contador = 0
+while (contador <= numeroDeAlunos) {
 
     if (contador== 0){
         console.log("O número atual é ZERO")
-    }else if (contador % 2 == 0){
-        console.log(`O número ${contador} é PAR`)
+    }else if (contador % 2 != 0){
+        console.log(`O número ${contador} é ÍMPAR`)
     }else {
-        console.log("O número "+ contador +" é IMPAR")
+        console.log("O número "+ contador +" é PAR")
     }
+    contador++
 }
 
