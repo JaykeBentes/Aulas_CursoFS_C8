1cc382a8d3190d76b78367c3c7e4da1c9600ae95
JaykeBentes <jakebentes@hotmail.com>
2022-10-24 18:24:09 -0400

    Atualização primeira parte da aula

--- Atividade5_UC8.js
+++ Atividade5_UC8.js
@@ -8,3 +8,15 @@ Se o número for ímpar, escreva ‘ímpar’ e o número correspondente
 Se o número for zero, escreva ‘zero’ e o número correspondente
 */
 
+let numeroDeAlunos = 12
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+
+    if (contador== 0){
+        console.log("O número atual é ZERO")
+    }else if (contador % 2 == 0){
+        console.log(`O número ${contador} é PAR`)
+    }else {
+        console.log("O número "+ contador +" é IMPAR")
+    }
+}
+
